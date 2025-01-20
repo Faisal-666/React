8fb01dfe4743107d51a6d1939b81131618337248
Faisal <faisalakbar661@gmail.com>
2025-01-20 12:43:18 +0700

    fix typo

--- jenkins/scripts/gh-pages.sh
+++ jenkins/scripts/gh-pages.sh
@@ -5,6 +5,6 @@ ls build
 git config --global user.email "faisalakbar661@gmail.com"
 git config --global user.name "Faisal"
 
-git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+git remote set-url origin https://Faisal-666:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
 npx gh-pages --message '[skip ci] updates' --dist build
