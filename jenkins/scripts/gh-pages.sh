ea47a96d1ae1550c5c98f7d760f62c0c00ec9356
Faisal <faisalakbar661@gmail.com>
2025-01-20 17:29:32 +0700

    fixing.....

--- jenkins/scripts/gh-pages.sh
+++ jenkins/scripts/gh-pages.sh
@@ -5,7 +5,7 @@ ls build
 
 git config --global user.email "faisalakbar661@gmail.com"
 git config --global user.name "Faisal"
-
+git config --global http.postBuffer 157286400
 git remote set-url origin https://Faisal-666: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@github.com/Faisal-666/React.git
 
 npx gh-pages --message '[skip ci] updates' --dist build
